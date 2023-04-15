1772d6545104b46eb2e5241cff82067ad8443d45
KwakYounHo <kwakyh951@gmail.com>
2023-04-15 23:18:16 +0900

    회원가입 성공 페이지 추가

---
+++ CreateAccount/SuccessCreate/successCreate.js
@@ -0,0 +1,5 @@
+import tagMaker from "../../src/modules/tagMaker.js";
+
+const root = tagMaker('div',document.body,{'id':'root'});
+tagMaker('h1',root,{},'축하합니다~ 회원가입에 성공하셨습니다~');
+tagMaker('a',root,{'href':'/Main.html'},'메인 페이지로');
