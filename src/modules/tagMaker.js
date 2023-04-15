842ce658ab6deb555a4a8e358428c1f482ca1c09
KwakYounHo <kwakyh951@gmail.com>
2023-04-15 20:36:51 +0900

    태그메이커 함수를 의도한 매개변수를 입력시 작동하도록 로직 변경

--- src/modules/tagMaker.js
+++ src/modules/tagMaker.js
@@ -1,18 +1,22 @@
 export default function (tagName,target,Attribute,innerText) {
   const element = document.createElement(tagName);
-  if (typeof(Attribute) === 'object') {
-    for (p in Attribute) {
-      element.setAttribute(p,Attribute[p]);
+  if (Attribute) {
+    if (typeof(Attribute) === 'object') {
+      for (let p in Attribute) {
+        element.setAttribute(p,Attribute[p]);
+      }
+    } else {
+      const NoObject = new Error('속성 매개변수는 객체 형식이어야 합니다.')
+      console.error(NoObject);
     }
-  } else {
-    const NoObject = new Error('속성 매개변수는 객체 형식이어야 합니다.')
-    console.error(NoObject);
   }
-  if (typeof(innerText) === 'string') {
-    element.innerText = innerText;
-  } else {
-    const NoString = new Error('innerText 매개변수는 문자열 형식이어야 합니다.')
-    console.error(NoString);
+  if (innerText) {
+    if (innerText && typeof(innerText) === 'string') {
+      element.innerText = innerText;
+    } else {
+      const NoString = new Error('innerText 매개변수는 문자열 형식이어야 합니다.')
+      console.error(NoString);
+    }
   }
   target.appendChild(element);
   return element
