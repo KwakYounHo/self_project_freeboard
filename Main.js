92a319421ccc41cc66865763dca71f7dd19c81d7
KwakYounHo <kwakyh951@gmail.com>
2023-04-15 23:14:30 +0900

    메인 페이지에 게시판링크,회원가입링크 입력

--- Main.js
+++ Main.js
@@ -1,4 +1,5 @@
 import tagMaker from './src/modules/tagMaker.js'
 
-const root    = tagMaker('div',document.body,{'id':'root'});
-const Goboard = tagMaker('a',root,{'href':'/Community/board.html'},'게시판');
+const root      = tagMaker('div',document.body,{'id':'root'});
+const boardList = tagMaker('a',root,{'href':'/Community/board/List.html'},'게시판');
+const SignUp    = tagMaker('a',root,{'href':'/CreateAccount/createAccount.html'},'회원가입');
