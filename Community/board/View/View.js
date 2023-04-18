e3d67f4e9c6ae134fae90ef0c7a0d744045a291f
KwakYounHo <kwakyh951@gmail.com>
2023-04-18 09:13:37 +0900

    뷰페이지 진입 시 글 제목으로 요청 보내기

--- Community/board/View/View.js
+++ Community/board/View/View.js
@@ -1,3 +1,9 @@
 import tagMaker from "../../../src/modules/tagMaker.js";
 
 const root = tagMaker('div',document.body,{'id':'root'});
+
+const RequestTitle     = document.location.href.split('/')[6];
+const boardTextRequest = new XMLHttpRequest;
+
+boardTextRequest.open('POST','?'+document.location.href);
+boardTextRequest.send();
