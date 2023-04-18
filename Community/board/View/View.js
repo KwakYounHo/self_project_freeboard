748fa0ce92bd949a9fe97e6e6dcf9b526c36b19d
KwakYounHo <kwakyh951@gmail.com>
2023-04-18 18:09:46 +0900

    게시판 뷰페이지 기능 구현 1차 완료

--- Community/board/View/View.js
+++ Community/board/View/View.js
@@ -12,16 +12,18 @@ const boardTextRequest = new XMLHttpRequest;
 boardTextRequest.open('GET','title='+RequestTitle);
 boardTextRequest.send();
 boardTextRequest.addEventListener('load',()=>{
-  const parsedData       = JSON.parse(boardTextRequest.response)
-  console.log(parsedData);
+  const parsedData       = JSON.parse(boardTextRequest.response)[0]
   const column_first_tr  = tagMaker('tr',thead);
-  const title            = tagMaker('th',column_first_tr,{'colspan':'4'});
-  title.innerText        = boardTextRequest.response.title;
+  const title            = tagMaker('th',column_first_tr,{'colspan':'4'},parsedData.title);
   const column_second_tr = tagMaker('tr',thead);
   const writed           = tagMaker('th',column_second_tr,{},'글쓴이');
-  const writed_value     = tagMaker('td',column_second_tr,boardTextRequest.response.writed);
-  const created          = tagMaker('th',column_second_tr,{},'작성자');
-  const created_value    = tagMaker('td',column_second_tr,boardTextRequest.response.created);
+  const writed_value     = tagMaker('td',column_second_tr,{},parsedData.writed);
+  const created          = tagMaker('th',column_second_tr,{},'생성일');
+  const created_value    = tagMaker('td',column_second_tr,{},parsedData.created.split('T')[0]);
   const textColumn       = tagMaker('tr',tbody);
-  const textValue        = tagMaker('td',textColumn,{},boardTextRequest.response.text);
+  const textBox_td       = tagMaker('td',textColumn,{'colspan':'4'});
+  const textbox          = tagMaker('div',textBox_td,{'id':'textBox'},`<p>${parsedData.text}</p>`);;
+  const table_footer     = tagMaker('tr',tbody);
+  const updated_th       = tagMaker('th',table_footer,{},'수정일');
+  const updated          = tagMaker('div',table_footer,{},parsedData.updated.split('T')[0]);
 })
