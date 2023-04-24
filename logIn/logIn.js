e22f07557127d66051fd7e7034c9eba5503b0775
KwakYounHo <kwakyh951@gmail.com>
2023-04-24 15:36:32 +0900

    로그인 버튼 추가(서브밋)

--- logIn/logIn.js
+++ logIn/logIn.js
@@ -1,18 +1,22 @@
 import tagMaker from '/src/modules/tagMaker.js'
 
-const root = tagMaker('div',document.body,{
+const root   = tagMaker('div',document.body,{
   'id':'root'
 });
-const form = tagMaker('form',root,{
+const form   = tagMaker('form',root,{
   'action':'/logIn/logInRequest','method':'POST'
 });
-const id   = tagMaker('input',form,{
+const id     = tagMaker('input',form,{
   'type'        : 'text',
   'name'        : 'ID',
   'placeholder' : '아이디를 입력해 주세요.'
 });
-const pw   = tagMaker('input',form,{
-  'tpye'        : 'password',
+const pw     = tagMaker('input',form,{
+  'type'        : 'password',
   'name'        : 'PW',
   'placeholder' : '비밀번호 입력'
+})
+const submit = tagMaker('input',form,{
+  'type'        : 'submit',
+  'value'       : '로그인'
 })
