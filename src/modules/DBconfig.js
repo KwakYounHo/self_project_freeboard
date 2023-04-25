75a54f51ae59a071e215f10bf44d629233938029
KwakYounHo <kwakyh951@gmail.com>
2023-04-25 13:46:33 +0900

    클래스를 이용한 DBconfig

--- src/modules/DBconfig.js
+++ src/modules/DBconfig.js
@@ -2,10 +2,24 @@ import mysql  from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config({path : '.env'});
 
-export default mysql.createConnection({
-  host     : process.env.mysqlHost,
-  user     : process.env.mysqlUser,
-  port     : process.env.mysqlPort,
-  password : process.env.mysqlPassword,
-  database : process.env.mysqlDatabase,
-});
+// export default mysql.createConnection({
+  // host     : process.env.mysqlHost,
+  // user     : process.env.mysqlUser,
+  // port     : process.env.mysqlPort,
+  // password : process.env.mysqlPassword,
+  // database : process.env.mysqlDatabase,
+// });
+
+class DBConnector{
+  constructor(user,password) {
+    this.host     = process.env.mysqlHost;
+    this.user     = user;
+    this.port     = process.env.mysqlPort;
+    this.password = password;
+    this.database = process.env.mysqlDatabase;
+  }
+}
+
+export default function (user, password) {
+  return mysql.createConnection(new DBConnector);
+}
